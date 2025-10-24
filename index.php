340264c3dd5f7754351fd554600f71807bea1293
LudvigNorberg07 <lmfnorberg@gmail.com>
2025-10-24 12:38:42 +0200

    startup

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <Header>Föreningen för Fågelskådning i Falsterbo</Header>
+
+    <div class="main">
+        <nav>
+            <a href="birds.html">Fåglar i Falsterbo</a><br>
+            <a href="facts.html">Fakta om fåglar</a><br>
+            <a href="join.html">Gå med i föreningen</a><br>
+            <a href="contact.html">Kontakt</a><br>
+            <a href="about.html">Om oss</a><br>
+        </nav>
+        <section>Bildspel</section>
+    </div>
+
+    <footer>&copy;Föreningen för Fågelskådning i Falsterbo</footer>
+</body>
+</html>
